18e93db3d7f33081fb408243afdb5d9c929a4733
Vaibhav Aggarwal <vaibhavagg12393@gmail.com>
2018-09-14 03:48:05 -0500

    Delete template tags directory

--- waterisland/home/templatetags/parse_details.py
+++
@@ -1,9 +0,0 @@
-from django import template
-
-register = template.Library()
-
-@register.filter
-def parse_details(parse_string):
-    parse_string = parse_string.replace('[', '')
-    parse_string = parse_string.replace(']', '')
-    return parse_string.split(',')
